1edd464f42c9bf4456ad20d027b865b6ffac1583
daniil <beafonin@gmail.com>
2026-02-10 13:05:22 +0100

    update t2

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,18 @@
 class Storage {
   constructor(items) {
-    this.items = items;
+    this.#items = items;
   }
 
   getItems() {
-    return this.items;
+    return this.#items;
   }
 
   addItem(item) {
-    this.items.push(item);
+    this.#items.push(item);
   }
 
   removeItem(item) {
-    this.items = this.items.filter(i => i !== item);
+    this.#items = this.#items.filter(i => i !== item);
   }
 }
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
